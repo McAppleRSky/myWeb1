7f689258fb16eef8a687b8ae8039d8499c9d7763
McAppleRSky <user0@DESKTOP-2AVECJ5.localdomain>
2020-02-27 17:52:48 +0300

    script

--- js/topMenuHandler.js
+++ js/topMenuHandler.js
@@ -7,17 +7,18 @@ icons[square] = "square.png";
 icons[caret] = "caret.png";
 icons[caretSquare] = "caret-square.png";
 
-function loaded(linkHref){
-  //console.log(linkHref);
+function loadedCurrentPage(linkHref){
+  //console.log(linkHref.split('#')[0]);
+  //console.log(window.location.href.split('#')[0]);
   result = false;
-  if (window.location.href==linkHref)
+  if (window.location.href.split('#')[0]==linkHref.split('#')[0])
     result = true;
   return result;
 }
 
 function ChangeOver(liMenu) {
   //console.log();
-  if (loaded(liMenu.getElementsByTagName('a')[0].href))
+  if (loadedCurrentPage(liMenu.getElementsByTagName('a')[0].href))
     liMenu.querySelector('.li-menu-img').src
     = 'img/icon/'+icons[square];
   else liMenu.querySelector('.li-menu-img').src
