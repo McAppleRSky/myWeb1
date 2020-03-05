8844d8b37feaa5e101a028808e293eca4fa23f5a
McAppleRSky <mcapple@yandex.ru>
2020-03-05 10:01:38 +0300

    Fix validation page5

--- js/page5valid.js
+++ js/page5valid.js
@@ -1,5 +1,7 @@
 var form // = document.querySelector('#messageForm')
 ;
+var submitStatus = false;
+var validadedElements = new Array();
 
 const telef_pattern = /(\+[3|7]\d{10})|\d{9}/;
 //  var telef_pattern_uni=/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
@@ -22,12 +24,37 @@ function qHowManyTextWord(message) {
   }
   return message.split(" ").length - dec; //можно было бы убирать дубли внутри
 }
+/**
+function howManyTextWord(str) {
+  var cnt = 0,
+  startpos = 0;
+  while (str.indexOf(" ", startpos) >= 0) {
+    cnt++;
+    // переведем стартовую  позицию для поиска на следующий символ
+    startpos = str.indexOf(" ", startpos) + 1;
+  }
+  // увеличим счетчик, если последний пробел стоит не в конце строки
+  if (startpos < str.length) {
+    cnt++;
+  }
+  return cnt;
+}
+*/
+function canSubmit(){
+  //console.log("test canSubmit");
+  let result = true;
+  for (var i = validadedElements.length; i --; ) {
+    if(validadedElements[i].classList.contains("valid")==false) result=false;
+  }
+  return result;
+}
 
 function fnsFocusinHandler() {
   let fns = form.familyNameSurname;
   fns.classList.add("focused");
   fns.classList.remove("valid");
   fns.classList.remove("invalid");
+  document.querySelector("#submit-button").disabled=true;
 }
 function fnsFocusoutHandler() {
   let fns = form.familyNameSurname;
@@ -78,6 +105,7 @@ function fnsFocusoutHandler() {
             } else {
               form.querySelector("#valid06").style.display = "none";
               fns.classList.add("valid");
+              if(canSubmit())document.querySelector("#submit-button").disabled=false;
             }
           }
         }
@@ -92,6 +120,7 @@ function genderFocusinHandler() {
   genderOutline.classList.add("focused");
   genderOutline.classList.remove("valid");
   genderOutline.classList.remove("invalid");
+  document.querySelector("#submit-button").disabled=true;
 }
 function genderFocusoutHandler() {
   let genderOutline = document.querySelector("#genderOutline");
@@ -105,6 +134,7 @@ function genderFocusoutHandler() {
   } else {
     form.querySelector("#valid11").style.display = "none";
     genderOutline.classList.add("valid");
+    if(canSubmit())document.querySelector("#submit-button").disabled=false;
   }
 
 }
@@ -114,6 +144,7 @@ function telefFocusinHandler() {
   telef.classList.add("focused");
   telef.classList.remove("valid");
   telef.classList.remove("invalid");
+  document.querySelector("#submit-button").disabled=true;
 }
 function telefFocusoutHandler() {
   let telef = form.telef;
@@ -135,6 +166,7 @@ function telefFocusoutHandler() {
       //console.log("telef valid");
       form.querySelector("#valid20").style.display = "none";
       telef.classList.add("valid");
+      if(canSubmit())document.querySelector("#submit-button").disabled=false;
     }
   }
 }
@@ -144,6 +176,7 @@ function mailFocusinHandler() {
   mail.classList.add("focused");
   mail.classList.remove("valid");
   mail.classList.remove("invalid");
+  document.querySelector("#submit-button").disabled=true;
 }
 function mailFocusoutHandler() {
   let mail = form.mail;
@@ -156,6 +189,7 @@ function mailFocusoutHandler() {
   } else {
     form.querySelector("#valid21").style.display = "none";
     mail.classList.add("valid");
+    if(canSubmit())document.querySelector("#submit-button").disabled=false;
   }
 }
 
@@ -164,6 +198,7 @@ function messageFocusinHandler() {
   message.classList.add("focused");
   message.classList.remove("valid");
   message.classList.remove("invalid");
+  document.querySelector("#submit-button").disabled=true;
 }
 function messageFocusoutHandler() {
   let message = form.message;
@@ -182,6 +217,7 @@ function messageFocusoutHandler() {
     } else {
       form.querySelector("#valid32").style.display = "none";
       message.classList.add("valid");
+      if(canSubmit())document.querySelector("#submit-button").disabled=false;
     }
   }
 }
@@ -191,6 +227,7 @@ function birthdayFocusinHandler() {
   birthday.classList.add("focused");
   birthday.classList.remove("valid");
   birthday.classList.remove("invalid");
+  document.querySelector("#submit-button").disabled=true;
 }
 function birthdayFocusoutHandler() {
   let birthday = form.birthdate;
@@ -204,33 +241,40 @@ function birthdayFocusoutHandler() {
   } else {
     form.querySelector("#valid21").style.display = "none";
     birthday.classList.add("valid");
+    if(canSubmit())document.querySelector("#submit-button").disabled=false;
   }
-
 }
 
 function initValidElements() {
   form = document.querySelector('#messageForm');
   let fns = form.familyNameSurname;
+  validadedElements.push(fns);
   fns.addEventListener("focusin", fnsFocusinHandler);
   fns.addEventListener("focusout", fnsFocusoutHandler);
   let genderRadios = document.getElementsByName("gender");
   let genderOutline = document.querySelector("#genderOutline");
+  validadedElements.push(genderOutline);
   for (let i = genderRadios.length; i--;) {
     genderRadios[i].addEventListener("focusin", genderFocusinHandler);
     genderRadios[i].addEventListener("focusout", genderFocusoutHandler);
   }
   let telef = form.telef;
+  validadedElements.push(telef);
   telef.addEventListener("focusin", telefFocusinHandler);
   telef.addEventListener("focusout", telefFocusoutHandler);
   let mail = form.mail;
+  validadedElements.push(mail);
   mail.addEventListener("focusin", mailFocusinHandler);
   mail.addEventListener("focusout", mailFocusoutHandler);
   let message = form.message;
+  validadedElements.push(message);
   message.addEventListener("focusin", messageFocusinHandler);
   message.addEventListener("focusout", messageFocusoutHandler);
   let birthday = form.birthdate;
+  validadedElements.push(birthday);
   birthday.addEventListener("focusin", birthdayFocusinHandler);
   birthday.addEventListener("focusout", birthdayFocusoutHandler);
+  document.querySelector("#submit-button").disabled=true;
 }
 
 function page5init() {
@@ -239,26 +283,8 @@ function page5init() {
   initValidElements(); //init valid script (js/page5valid.js)
 }
 
-function howManyTextWord(str) {
-  var cnt = 0,
-    startpos = 0;
-  while (str.indexOf(" ", startpos) >= 0) {
-    cnt++;
-    // переведем стартовую  позицию для поиска на следующий символ
-    startpos = str.indexOf(" ", startpos) + 1;
-  }
-  // увеличим счетчик, если последний пробел стоит не в конце строки
-  if (startpos < str.length) {
-    cnt++;
-  }
-  return cnt;
-}
 
 function validMessageForm() {
-  var form = document.querySelector('#messageForm');
-  var submitMessage = document.querySelector('#MessageFormSubmitedMessage');
-  var submitStatus = true;
-
 //  var fns = form.familyNameSurname.value;
   //  var fnsWordTextCnt = qHowManyTextWord(form.familyNameSurname.value);
   //console.log(`howManyTextWord: ${howManyTextWord(form.familyNameSurname.value)}`);
@@ -268,6 +294,10 @@ function validMessageForm() {
 //  var message = form.message.value;
 //  var telef = form.telef.value;
 
+  var form = document.querySelector('#messageForm');
+  var submitMessage = document.querySelector('#MessageFormSubmitedMessage');
+  submitStatus = true;
+
   if (submitStatus) {
     form.style.display = "none";
     submitMessage.style.display = "inline";
@@ -275,5 +305,4 @@ function validMessageForm() {
   }
 
   return submitStatus;
-
 }
