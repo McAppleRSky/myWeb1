4918f3477f93d225c3e5b76e026dd040ecc43d9e
McAppleRSky <mcapple@yandex.ru>
2020-03-23 17:49:29 +0300

    chenge structure handler page4 for file & http protocols

--- js/page4handler.js
+++ js/page4handler.js
@@ -10,7 +10,8 @@ var lists = [],
   photos = [];
 
 var mainArticle,
-  full, formated, i;
+  full, formated, OnesReadystatechange = 0,
+  i;
 
 let myConsole = window.console;
 
@@ -24,32 +25,50 @@ function Photo(file, title, width, height) {
 document.onreadystatechange = fillListsAndPhotosAndRunHandler;
 
 function fillListsAndPhotosAndRunHandler() {
-  let curListValueCount,
-    imgList = document.querySelector("#imgList");
+  let imgList = document.querySelector(".imgList");
+  mainArticle = document.querySelector(".mainArticle");
   if (document.readyState == "complete") {
-    for (i = 0; i < listCount; i++) {
-      try {
-        lists[i] = window.frames[i].document
-          .querySelector("pre").textContent.split("\n");
-      } catch (e) {
-        myConsole.log(`list${i} with exception| ${e}`);
-      } finally {
-        curListValueCount = lists[i][0].split(';').length;
-        if (curListValueCount == 4) {
-          formated = i;
+    if (window.location.protocol == "http:") {
+      fillListsFromHttp();
+      mainArticle.removeChild(imgList);
+      fillPhotos();
+      runHandler();
+    }
+  } else {
+    if (window.location.protocol == "file:") {
+      myConsole.log("protocol is file");
+      while(window.frames.length<2){
+        setTimeout("go()",1000);
+        console.log(window.frames.length);
+        //lists[0] = window.frames[0];
+      }
+    } else {
+      myConsole.log("invalid protocol");
+    }
+  }
+}
+
+
+function fillListsFromHttp() {
+  let curListValueCount;
+  for (i = 0; i < listCount; i++) {
+    try {
+      lists[i] = window.frames[i].document
+        .querySelector("pre").textContent.split("\n");
+    } catch (e) {
+      myConsole.log(`list${i} with exception| ${e}`);
+    } finally {
+      curListValueCount = lists[i][0].split(';').length;
+      if (curListValueCount == 4) {
+        formated = i;
+      } else {
+        if (curListValueCount == 1) {
+          full = i;
         } else {
-          if (curListValueCount == 1) {
-            full = i;
-          } else {
-            myConsole.log("incorrect files list.txt listf.txt");
-          }
+          myConsole.log("incorrect files list.txt listf.txt");
         }
       }
     }
-    //imgList.style.display = "none";
-    fillPhotos();
-    runHandler();
-    //mainArticle = document.querySelector("#mainArticle");
   }
 }
 
@@ -73,15 +92,18 @@ function fillPhotos() {
       }
     }
   }
+  for (i = 0; i < lists[full].length; i++) {
+    if (lists[full][i] != "") {
+      photos.push(new Photo(lists[full][i], lists[full][i], "100", "100"));
+    }
+  }
 }
 
 function runHandler() {
-
   let photoTable,
     cellCount,
     tableWidthPercent,
-    newRow, newCell,
-    j, k;
+    newRow, newCell, fileName, anchEl, figEl, imgEl, figcapEl, j, k;
 
   //mainArticle = document.querySelector("#mainArticle")
 
@@ -89,66 +111,44 @@ function runHandler() {
 
   photoTable.id = "photoTable";
   cellCount = 4;
-  tableWidthPercent = '78';
-  photoTable.setAttribute('width', "" + tableWidthPercent + "%");
+  tableWidthPercent = "100";
+  photoTable.setAttribute("width", "" + tableWidthPercent + "%");
   i = 0;
   k = 0;
-  //console.log("i : "+i+", k : "+k);
-  //console.log(photos.length);
   while (k < photos.length) {
-    //console.log("i : "+i);
-    newRow = photoTable.insertRow(i++);
+    newRow = photoTable.insertRow(i);
     for (j = 0; j < cellCount; j++) {
-      //console.log("j : "+j+", k : "+k);
       newCell = newRow.insertCell(-1);
       if (k < photos.length) {
         newCell.width = "" + tableWidthPercent / cellCount;
         newCell.style.textAlign = "center";
-        newCell.innerHTML("");
-        //        newCell.innerHTML = "<a href=\"img/" +
-        //          photos[k].file +
-        //"\" target=\"_blank\"> <figure><img src=\"img/" +
-        //          photos[k].file + "\" width=\"" + photos[k].width + "\" " +
-        //          "height=\"" + photos[k].height + "\"><figcaption>" +
-        //          photos[k++].title + "</figcaption></figure></a>";
+
+        fileName = imgListPath + photos[k].file;
+
+        imgEl = document.createElement("img");
+        imgEl.setAttribute("src", fileName);
+        imgEl.setAttribute("width", photos[k].width);
+        imgEl.setAttribute("height", photos[k].height);
+
+        figcapEl = document.createElement("figcaption");
+        figcapEl.textContent = photos[k].title;
+        figcapEl.setAttribute("class", "figcapTitle");
+
+        figEl = document.createElement("figure");
+        figEl.appendChild(imgEl);
+        figEl.appendChild(figcapEl);
+
+        anchEl = document.createElement("a");
+        anchEl.setAttribute("href", fileName);
+        anchEl.setAttribute("target", "_blank");
+        anchEl.appendChild(figEl);
+
+        newCell.appendChild(anchEl);
+
+        k++;
       }
     }
-    //i++;
+    i++;
   }
-
   mainArticle.appendChild(photoTable);
 }
-
-//document.addEventListener("DOMContentLoaded", runHandler);
-//window.onload = runHandler();
-/**
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("DOMContentLoaded");
-  runHandler();
-});
-*/
-
-/**
-function ifListLoaded(i) {
-  console.log(`ifListLoaded ${i}`);
-  arrayFlagIsObjectLoaded[i] = true;
-  var counter;
-  for (i = listsCount, counter = 0; i--;) {
-    if (arrayFlagIsObjectLoaded[i] == true) counter++;
-  }
-  if (counter == listsCount) {
-    console.log(`counter(${counter}) listsCount(${listsCount})`);
-    runHandler();
-  }
-}
-*/
-
-
-/**
-function onloadFrame(myFrame){
-  console.log('Д');
-  console.log(`myFrame(${myFrame.childNodes[0]})`);
-  frame1 = this;
-  //myFrame.querySelector("pre");
-}
-*/
