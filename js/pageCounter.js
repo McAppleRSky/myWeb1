7f09c293e9a93f7ca4ae41d27268faaad2bba086
McAppleRSky <user0@DESKTOP-2AVECJ5.localdomain>
2020-05-03 00:22:33 +0300

    история сеанса (sessionStorage)

--- js/pageCounter.js
+++ js/pageCounter.js
@@ -1,10 +1,11 @@
 //var pageCount;
 prepCalcThisPage();
+sessionStoragePrepCalcThisPage();
 
 //deleteCookie(htmPage);
 //deleteCookie(htmPage + "StartSession");
 calcThisPage();
-
+sessionStorageCalcThisPage();
 //mainArticle = document.querySelector("#mainArticle")
 
 function prepCalcThisPage(){
@@ -19,16 +20,38 @@ function prepCalcThisPage(){
 //  console.log("prepCalc "+getCookie(htmPage));
 };
 
+function sessionStoragePrepCalcThisPage(){
+  //console.log("prepCalcThisPage on " + htmPage);
+  //let htmPageStr = getCookie(htmPage);
+  if (sessionStorage.getItem(htmPage) == null){
+    sessionStorage.setItem(htmPage, '', 1);
+  }
+  if(sessionStorage.getItem(htmPage) == ''){
+    sessionStorage.setItem(htmPage, '0', 1);
+  }
+//  console.log("prepCalc "+getCookie(htmPage));
+};
+
 function calcThisPage(){
   let pageCount = Number(getCookie(htmPage));
-  let date = new Date(Date.now());
   if(pageCount >= 0){
     if(pageCount == 0){
-      setCookie(htmPage+"TimeStamp", date.toUTCString(), 1);
+      let date = new Date(Date.now());
+      setCookie(htmPage + "TimeStamp", date.toUTCString(), 1);
     }
     setCookie(htmPage, pageCount + 1, 1);
   }
   //console.log(getCookie(htmPage));
 }
 
-//function sessionStartStamp(){console.log("sessionStartStamp run");}
+function sessionStorageCalcThisPage(){
+  let pageCount = Number(sessionStorage.getItem(htmPage));
+  if(pageCount >= 0){
+    if(pageCount == 0){
+      let date = new Date(Date.now());
+      sessionStorage.setItem(htmPage + "TimeStamp", date.toUTCString());
+    }
+    sessionStorage.setItem(htmPage, pageCount + 1);
+  }
+  //console.log(getCookie(htmPage));
+}
