6abb2b06a64ea4264643fe65def7a55d6c20d256
McAppleRSky <user0@DESKTOP-2AVECJ5.localdomain>
2020-06-10 15:48:13 +0300

    gettime.js with jquery

--- js/gettime.js
+++ js/gettime.js
@@ -1,21 +1,34 @@
-var periodDateTextNode = document.querySelector('#gobartable').rows[0].cells[0].getElementsByTagName('div')[0];
-var currentDateTextNode = document.querySelector('#gobartable').rows[1].cells[0].getElementsByTagName('div')[0];
-var beginDateTextNode = document.getElementsByTagName('time')[0];
-var beginDateParts = beginDateTextNode.dateTime.split('-');
-var dayNames = "воскресенье_понедельник_вторник_среда_четверг_пятница_суббота";
-var month = "";
+var dayNames = "воскресенье_понедельник_вторник_среда_четверг_пятница_суббота",
+  month = "",
+  beginDateParts = "",
+  currentDate;
 
-go();
 function go() {
-    var currentDate = new Date();
-    if(currentDate.getMonth()<10)month="0"+currentDate.getMonth();
-    else month=currentDate.getMonth();
-    currentDateTextNode.textContent
-        = `Сегодня: ${(""+currentDate).split(' ')[2]}.${month}.${currentDate.getYear()-100} г., ${
-        dayNames.split('_')[currentDate.getDay()]}, ${(""+currentDate).split(' ')[4]}`;
-    beginDate
-        = new Date(beginDateParts[0], (beginDateParts[1] - 1), beginDateParts[2]);
-    periodDateTextNode.textContent
-        = `Period development : ${((currentDate - beginDate) / (1000 * 3600 * 24)).toFixed()} days`;
-    setTimeout("go()",1000);
+  currentDate = new Date();
+  if (currentDate.getMonth() < 10) month = "0" + currentDate.getMonth();
+  else month = currentDate.getMonth();
+  $('#tDateBar tr:last td div')
+    .empty()
+    .append(`Сегодня: ${(""+currentDate).split(' ')[2]}.${month}.${currentDate.getYear()-100} г., ${
+        dayNames.split('_')[currentDate.getDay()]}, ${(""+currentDate).split(' ')[4]}`);
+  beginDate
+    = new Date(beginDateParts[0], (beginDateParts[1] - 1), beginDateParts[2]);
+  $('#tDateBar tr:first td div')
+    .empty()
+    .append(`Period development : ${((currentDate - beginDate) / (1000 * 3600 * 24)).toFixed()} days`);
+  setTimeout("go()", 1000);
 }
+
+$(function() {
+  $("#tDateBar")
+    .width("100%")
+    .css("border", "0");
+  $("#tDateBar tr:first")
+    .width("95%")
+    .css("text-align", "left");
+  $("#tDateBar tr:last")
+    .css("background-color", "gray")
+    .css("color", "white");
+  beginDateParts = $("time").attr("datetime").split('-');
+  go();
+});
