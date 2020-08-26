786776037d16a9095b8f3ba4f42cc173933c445e
McAppleRSky <mcapple@yandex.ru>
2020-08-26 09:19:11 +0300

    edit page4.htm 4template

--- js/TDD-read-file.js
+++
@@ -1,4 +0,0 @@
-filelistObject = document.createElement("object");
-filelistObject.setAttribute("data","img/list");
-filelistObject.setAttribute("type","text/plain");
-document.appendChild(filelistObject);
